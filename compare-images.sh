bee5d94482f6eaa5ca8537ba1a120cffbecbbc2f
T9Air <163940743+T9Air@users.noreply.github.com>
2024-06-28 13:05:25 -0400

    Update compare-images.sh

--- compare-images.sh
+++ compare-images.sh
@@ -1,3 +1,11 @@
-python3 ~/home/user/3d_print_error_detector/image-compare.py
+python3 ~/3d_print_error_detector/image-compare.py 12
 
-## change "user" in path to whatever your username is and then delete this line
+python_script_return_code=$?
+
+
+if [ $python_script_return_code -eq 0 ]; then
+  echo "No movement detected. Continue print."
+else
+  echo "PRINT MOVED FROM ORIGINAL SPOT! STOP NOW!"
+  echo "!klipper; gcode_macro check_macro_docs" > /tmp/klipper_fifo
+fi
